741ef23a3ba6646a80fa7c09252b4e0ca13a86c5
HannaBM12 <Hannabmulugeta@gmail.com>
2021-03-21 21:46:55 -0400

    commit tree 5785cfd755b2b6c69af4da3bdd1e2f718a272882

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+
+
+10.times do 
+    Post.create(title: Faker::Quote.yoda, content: Faker::Quote.matz)
+end
+
+10.times do
+    User.create(username: Faker::Internet.username, email: Faker::Internet.free_email)
+end
+10.times do
+    Comment.create(content: Faker::Fantasy::Tolkien.race, 
+            user: User.all.sample, post: Post.all.sample)
+end
